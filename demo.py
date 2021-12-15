7364695df5f02cf4c999f90ec03e299c55216357
Pushy <support@pushy.me>
2021-12-15 01:07:05 -0500

    demo.py: add Pushy Demo App ID and testing instructions

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ pushy.listen()
 
 try:
     # Register device for push notifications
-    deviceToken = pushy.register({ 'appId': 'YOUR_APP_ID' })
+    deviceToken = pushy.register({ 'appId': '550ee57c5b5d72117f51e801' })
     
     # Persist the device token in your backend database
 except Exception as err:
@@ -24,3 +24,6 @@ pushy.setNotificationListener(backgroundNotificationListener)
 
 # Avoid program termination (keeps your Python app from terminating until you call pushy.disconnect())
 pushy.loop_forever()
+
+# Send yourself a test notification from the demo page
+# https://pushy.me/docs/resources/demo
