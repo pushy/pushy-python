fb5f9e0cb6fc521bbd422aa7138c5337fa593ce0
Pushy <support@pushy.me>
2022-06-29 21:25:09 +0300

    Update link to GitHub repository

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='The official Pushy SDK for Python apps.',
     long_description=(pathlib.Path(__file__).parent / 'README.md').read_text(),
     long_description_content_type='text/markdown',
-    url='https://github.com/pushy-me/pushy-python',
+    url='https://github.com/pushy/pushy-python',
     author='Pushy',
     author_email='support@pushy.me',
     license='Apache 2.0',
